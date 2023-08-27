1af802966e03f63daf5645b290901ebaa65803b6
jusylopes <jusylopes@outlook.com>
2023-08-27 00:47:26 -0300

    add: test bmi

---
+++ test/bmi_teste.dart
@@ -0,0 +1,13 @@
+import 'package:bmi_calculator/models/person.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Retorna BMI', () {
+    expect(calculteBMI(Person('João', 20, 1.75)), 6.530612244897959);
+  });
+
+  test('Retorna classificação BMI', () {
+    double bmi = calculteBMI(Person('João', 5, 1.75));
+    expect(printBMICategory(bmi), 'Magreza grave');
+  });
+}
