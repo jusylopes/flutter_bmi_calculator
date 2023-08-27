0b83b5092d65c0c6fe462f0eb1a78136b79138bb
jusylopes <jusylopes@outlook.com>
2023-08-27 00:47:39 -0300

    add: model person

--- test/bmi_teste.dart
+++ test/bmi_teste.dart
@@ -6,8 +6,8 @@ void main() {
     expect(calculteBMI(Person('João', 20, 1.75)), 6.530612244897959);
   });
 
-  test('Retorna classificação BMI', () {
-    double bmi = calculteBMI(Person('João', 5, 1.75));
+  test('Retorna classificação IMC', () {
+    double bmi = calculteBMI(Person('Amanda', 5, 1.75));
     expect(printBMICategory(bmi), 'Magreza grave');
   });
 }
