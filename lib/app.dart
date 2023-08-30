45d0fcafec80fea5cae38ab296e9f08fe5194cb0
jusylopes <jusylopes@outlook.com>
2023-08-30 20:54:02 -0300

    refactor: bmi provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:bmi_calculator/providers/bmi_provider.dart';
-import 'package:bmi_calculator/screens/splash_screen.dart';
+import 'package:bmi_calculator/screens/gender_screen.dart';
+
+import 'package:bmi_calculator/utils/constants.dart';
 import 'package:bmi_calculator/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -10,11 +12,11 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => BmiProvider(),
+      create: (context) => BmiProvider(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'BMI Calculator',
-        home: const SplashScreen(),
+        title: Constants.appName,
+        home: const GenderScreen(),
         theme: AppTheme.light,
       ),
     );
--- lib/providers/bmi_provider.dart
+++ lib/providers/bmi_provider.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator/models/bmi_model.dart';
 
-class BmiProvider extends ChangeNotifier {
-  double _bmi = 0;
-  double get bmi => _bmi;
+class BmiProvider with ChangeNotifier {
+  final BmiModel _bmiModel = BmiModel(
+    height: 160,
+    weight: 60,
+    gender: Gender.NULL,
+    isSelectedGender: false,
+  );
 
-  void calculateBmi({required BmiModel bmiModel}) {
-    _bmi = bmiModel.weight / (bmiModel.height * bmiModel.height);
+  double get height => _bmiModel.height;
+  double get weight => _bmiModel.weight;
+  Gender get selectedGender => _bmiModel.gender;
+
+  double get bmiValue =>
+      _bmiModel.weight / (_bmiModel.height * _bmiModel.height);
+
+  void updateHeight(double height) {
+    _bmiModel.height = height;
     notifyListeners();
+  }
+
+  void updateWeight(double weight) {
+    _bmiModel.weight = weight;
+    notifyListeners();
+  }
+
+  void selectGender(Gender gender) {
+    _bmiModel.gender = gender;
+    notifyListeners();
+  }
+
+  String getResult() {
+    double bmi = bmiValue;
+
+    if (bmi < 16) {
+      return '';
+    } else {
+      return 'Obeso';
+    }
   }
 }
