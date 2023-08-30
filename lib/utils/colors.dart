b35aa982377a1b27db31baf8c43ce76924b66523
jusylopes <jusylopes@outlook.com>
2023-08-30 20:51:37 -0300

    add: theme

---
+++ lib/utils/colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static MaterialColor createMaterialColor(Color color) {
+    List<Color> shades = [
+      color.withOpacity(0.1),
+      color.withOpacity(0.2),
+      color.withOpacity(0.3),
+      color.withOpacity(0.4),
+      color.withOpacity(0.5),
+      color.withOpacity(0.6),
+      color.withOpacity(0.7),
+      color.withOpacity(0.8),
+      color.withOpacity(0.9),
+      color,
+    ];
+
+    return MaterialColor(color.value, {
+      50: shades[0],
+      100: shades[1],
+      200: shades[2],
+      300: shades[3],
+      400: shades[4],
+      500: shades[5],
+      600: shades[6],
+      700: shades[7],
+      800: shades[8],
+      900: shades[9],
+    });
+  }
+
+  static MaterialColor colorPrimarySwatch = createMaterialColor(primaryColor);
+  
+  static const Color colorBackground = Color(0xFF16A9B5);
+  static const Color primaryColor = Color(0xFFf8ab33);
+}
--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,26 +1,39 @@
+import 'package:bmi_calculator/utils/colors.dart';
 import 'package:flutter/material.dart';
-
-Map<int, Color> _color = {
-  50: const Color.fromRGBO(90, 4, 8, .1),
-  100: const Color.fromRGBO(90, 4, 8, .2),
-  200: const Color.fromRGBO(90, 4, 8, .3),
-  300: const Color.fromRGBO(90, 4, 8, .4),
-  400: const Color.fromRGBO(90, 4, 8, .5),
-  500: const Color.fromRGBO(90, 4, 8, .6),
-  600: const Color.fromRGBO(90, 4, 8, .7),
-  700: const Color.fromRGBO(90, 4, 8, .8),
-  800: const Color.fromRGBO(90, 4, 8, .9),
-  900: const Color.fromRGBO(90, 4, 8, 1),
-};
-
-MaterialColor _colorCustom = MaterialColor(0xFFE50914, _color);
+import 'package:google_fonts/google_fonts.dart';
 
 abstract class AppTheme {
   static ThemeData get light => ThemeData(
-      primarySwatch: _colorCustom,
-      visualDensity: VisualDensity.adaptivePlatformDensity,
-      scaffoldBackgroundColor: Colors.white,
-      textTheme: TextTheme(
-        titleLarge: TextStyle(fontSize: 30.0, color: _colorCustom),
-      ));
+        primarySwatch: AppColors.colorPrimarySwatch,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: GoogleFonts.robotoCondensed().fontFamily,
+        scaffoldBackgroundColor: AppColors.colorBackground,
+        appBarTheme: AppBarTheme(
+          elevation: 0,
+          centerTitle: true,
+          toolbarHeight: 100,
+          backgroundColor: Colors.transparent,
+          titleTextStyle: TextStyle(
+              fontFamily: GoogleFonts.robotoCondensed().fontFamily,
+              fontSize: 33.0,
+              color: Colors.black,
+              fontWeight: FontWeight.bold),
+        ),
+        textTheme: _buildTextTheme(),
+      );
+
+  static TextTheme _buildTextTheme() {
+    return const TextTheme(
+      titleLarge: TextStyle(
+        fontSize: 38.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 22.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+    );
+  }
 }
