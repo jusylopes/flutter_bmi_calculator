8c7b4ad70dbce3865710963cd1b1a685805cb50d
jusylopes <jusylopes@outlook.com>
2023-08-31 13:27:39 -0300

    add: gender widget

---
+++ lib/screens/home/widgets/image_gender.dart
@@ -0,0 +1,39 @@
+import 'package:bmi_calculator/models/bmi_model.dart';
+import 'package:bmi_calculator/providers/bmi_provider.dart';
+import 'package:flutter/material.dart';
+
+class ImageGender extends StatelessWidget {
+  const ImageGender({
+    super.key,
+    required this.bmiProvider,
+    required this.gender,
+    required this.assetImage,
+  });
+
+  final BmiProvider bmiProvider;
+  final Gender gender;
+  final String assetImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => bmiProvider.selectGender(gender),
+      onDoubleTap: () => bmiProvider.selectGender(Gender.NULL),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width / 2.5,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              assetImage,
+              color: bmiProvider.selectedGender == gender ||
+                      bmiProvider.selectedGender == Gender.NULL
+                  ? null
+                  : const Color.fromRGBO(255, 255, 255, 0.5),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
