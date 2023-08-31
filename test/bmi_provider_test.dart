a8867e91c6d171ba43c1714c3006ea2a1dfc9f5f
jusylopes <jusylopes@outlook.com>
2023-08-31 13:26:23 -0300

    refactor: bmi provider test

--- test/bmi_provider_test.dart
+++ test/bmi_provider_test.dart
@@ -1,32 +1,14 @@
 import 'package:bmi_calculator/providers/bmi_provider.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-// void main() {
-//   group('BmiProvider', () {
-//     test('calculateBmi calculates BMI correctly', () {
-//       final bmiProvider = BmiProvider();
-//       double heightPerson = 1.75;
-//       const double weightPerson = 70;
+void main() {
+  group('BmiProvider', () {
+    test('calculateBmi calculates BMI correctly', () {
+      final bmiProvider = BmiProvider();
+      bmiProvider.updateHeight(170);
+      bmiProvider.updateWeight(70);
 
-//       bmiProvider.calculateBmi(height: heightPerson, weight: weightPerson);
-
-//       expect(bmiProvider, closeTo(22.86, 0.01));
-//     });
-
-//     test('calculateBmi notifies listeners', () {
-//       final bmiProvider = BmiProvider();
-
-//       double heightPerson = 1.75;
-//       const double weightPerson = 70;
-
-//       int listenerCallCount = 0;
-//       bmiProvider.addListener(() {
-//         listenerCallCount++;
-//       });
-
-//       bmiProvider.calculateBmi(height: heightPerson, weight: weightPerson);
-
-//       expect(listenerCallCount, 1);
-//     });
-//   });
-// }
+      expect(bmiProvider.bmiValue, closeTo(24.22, 0.01));
+    });
+  });
+}
