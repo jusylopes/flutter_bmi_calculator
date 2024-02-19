2086ec85e943325fdb451eeb73e6b07a0fde885f
jusylopes <jusylopes@outlook.com>
2024-02-19 15:42:46 -0300

    add: image caracter

--- lib/view/components/image_caracter.dart
+++ lib/view/components/image_caracter.dart
@@ -2,29 +2,29 @@ import 'package:bmi_calculator/models/bmi_model.dart';
 import 'package:bmi_calculator/controllers/bmi_controller.dart';
 import 'package:flutter/material.dart';
 
-class ImageGender extends StatelessWidget {
-  const ImageGender({
+class ImageCaracter extends StatelessWidget {
+  const ImageCaracter({
     super.key,
     required this.bmiController,
-    required this.gender,
+    required this.caracter,
     required this.assetImage,
   });
 
   final BmiController bmiController;
-  final Gender gender;
+  final Caracter caracter;
   final String assetImage;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => bmiController.selectGender(gender),
-      onDoubleTap: () => bmiController.selectGender(Gender.NULL),
+      onTap: () => bmiController.selectCaracter(caracter),
+      onDoubleTap: () => bmiController.selectCaracter(Caracter.NULL),
       child: SizedBox(
         width: MediaQuery.of(context).size.width / 2.5,
         child: Image.asset(
           assetImage,
-          color: bmiController.selectedGender == gender ||
-                  bmiController.selectedGender == Gender.NULL
+          color: bmiController.selectedCaracter == caracter ||
+                  bmiController.selectedCaracter == Caracter.NULL
               ? null
               : const Color.fromRGBO(255, 255, 255, 0.5),
         ),
