3a07f9ae4ddb4e5c74f24b60e979e1925d31ed95
jusylopes <jusylopes@outlook.com>
2024-02-20 01:29:39 -0300

    refactor: home screen

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:bmi_calculator/models/bmi_model.dart';
 import 'package:bmi_calculator/controllers/bmi_controller.dart';
 import 'package:bmi_calculator/utils/snackbar_utils.dart';
 import 'package:bmi_calculator/view/bmi_screen.dart';
-import 'package:bmi_calculator/view/components/image_gender.dart';
+import 'package:bmi_calculator/view/components/image_caracter.dart';
 import 'package:bmi_calculator/utils/assets_manager.dart';
 import 'package:bmi_calculator/view/favorite_screen.dart';
 import 'package:flutter/material.dart';
@@ -17,15 +17,16 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Select your gender',
+        title: const Text('Selecione seu personagem',
             maxLines: 2, textAlign: TextAlign.center),
         actions: [
           IconButton(
             icon: const Icon(Icons.arrow_forward_ios),
             onPressed: () {
-              bmiController.selectedGender == Gender.NULL
+              bmiController.selectedCaracter == Caracter.NULL
                   ? SnackbarUtils.showSnackBar(
-                      context: context, text: 'Please select a gender')
+                      context: context,
+                      text: 'Por favor, selecione um personagem!')
                   : Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -39,7 +40,7 @@ class HomeScreen extends StatelessWidget {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => const FavoriteScreen()),
+              MaterialPageRoute(builder: (context) => FavoriteScreen()),
             );
           },
         ),
@@ -48,17 +49,17 @@ class HomeScreen extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ImageGender(
+            ImageCaracter(
               bmiController: bmiController,
-              gender: Gender.FEMALE,
+              caracter: Caracter.FEMALE,
               assetImage: AssetsManager.imageFemale,
             ),
             const SizedBox(
               width: 10,
             ),
-            ImageGender(
+            ImageCaracter(
               bmiController: bmiController,
-              gender: Gender.MALE,
+              caracter: Caracter.MALE,
               assetImage: AssetsManager.imageMale,
             )
           ],
