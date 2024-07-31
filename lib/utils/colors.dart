c747f327db468b1a2adcf37c08261d7097c5a106
jusylopes <jusylopes@outlook.com>
2024-07-30 21:22:53 -0300

    style: update app colors to use hex values

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,39 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFFF4A20F);
-  static MaterialColor createMaterialColor(Color color) {
-    List<Color> shades = [
-      color.withOpacity(0.1),
-      color.withOpacity(0.2),
-      color.withOpacity(0.3),
-      color.withOpacity(0.4),
-      color.withOpacity(0.5),
-      color.withOpacity(0.6),
-      color.withOpacity(0.7),
-      color.withOpacity(0.8),
-      color.withOpacity(0.9),
-      color,
-    ];
+  static const Color primaryColor = Color(0xFF6750a4);
+  static const Color seedColor = Color(0xE6F6BF1D);
 
-    return MaterialColor(color.value, {
-      50: shades[0],
-      100: shades[1],
-      200: shades[2],
-      300: shades[3],
-      400: shades[4],
-      500: shades[5],
-      600: shades[6],
-      700: shades[7],
-      800: shades[8],
-      900: shades[9],
-    });
-  }
-
-  static MaterialColor colorPrimarySwatch = createMaterialColor(primaryColor);
-  static const Color colorBackground = Color(0xFFF4A20F);
-  static const Color colorBmiGreen = Color.fromRGBO(10, 115, 119, 0.9);
-  static const Color colorBmiYellow = Color.fromRGBO(246, 191, 29, 0.9);
-  static const Color colorBmiRed = Color.fromRGBO(190, 44, 62, 0.9);
-  static const Color colorBmiOrange = Color.fromRGBO(246, 116, 29, 0.9);
+  static const Color colorScaffoldBackground = Color(0xFFF4A20F);
+  static const Color colorBmiGreen = Color(0xE60A7377);
+  static const Color colorBmiYellow = Color(0xE6F6BF1D);
+  static const Color colorBmiRed = Color(0xE6BE2C3E);
+  static const Color colorBmiOrange = Color(0xE6F6741D);
 }
