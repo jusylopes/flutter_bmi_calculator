8746e5ac7494b8c8d75fc1ee9cb3d25cbeb51f79
jusylopes <jusylopes@outlook.com>
2024-08-17 00:05:53 -0300

    feat: Update character images and animations in BMI screen

--- lib/view/components/animation_character_bmi.dart
+++ lib/view/components/animation_character_bmi.dart
@@ -1,24 +1,35 @@
 import 'package:bmi_calculator/models/character.dart';
 import 'package:bmi_calculator/utils/assets_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
+import 'package:rive/rive.dart' as rive;
 
 class AnimationCharacterBmi extends StatelessWidget {
   const AnimationCharacterBmi({
-    Key? key,
+    super.key,
     required this.character,
-  }) : super(key: key);
+  });
 
   final Character character;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height / 1.7,
-      width: MediaQuery.of(context).size.width / 1.8,
-      child: RiveAnimation.asset(
-        AssetsManager.getCharacterAsset(character, true),
-      ),
+    return Stack(
+      alignment: Alignment.bottomLeft,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 50),
+          child: Image.asset(
+            AssetsManager.scale,
+            height: MediaQuery.of(context).size.height / 10,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 40),
+          child: rive.RiveAnimation.asset(
+            AssetsManager.getCharacterAsset(character),
+          ),
+        ),
+      ],
     );
   }
 }
